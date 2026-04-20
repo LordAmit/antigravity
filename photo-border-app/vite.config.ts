c073fb44ee646c65a0e900b7bd95cbbdf4f50853
Amit Seal Ami <lordamit@gmail.com>
2026-04-20 13:40:32 -0400

    pwa

--- photo-border-app/vite.config.ts
+++ photo-border-app/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
       manifest: {
         name: 'Borderify Images',
         short_name: 'Borderify',
